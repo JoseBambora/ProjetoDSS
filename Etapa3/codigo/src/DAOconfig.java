1185d561c6435542aaa3d24d40b08a4a0e43e767
JoseBambora <zebragapt@gmail.com>
2022-12-11 11:24:19 +0000

    Adding DAO configuration.

---
+++ Etapa3/codigo/src/DAOconfig.java
@@ -0,0 +1,10 @@
+public class DAOconfig {
+    static final String USERNAME = "";                       // Actualizar
+    static final String PASSWORD = "";                       // Actualizar
+    private static final String DATABASE = "simuladorDSS";          // Actualizar
+    //private static final String DRIVER = "jdbc:mariadb";        // Usar para MariaDB
+    private static final String DRIVER = "jdbc:mysql";        // Usar para MySQL
+    static final String URL = DRIVER+"://localhost:3306/"+DATABASE+"?allowPublicKeyRetrieval=true&useSSL=false";
+
+
+}
