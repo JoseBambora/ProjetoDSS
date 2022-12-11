a30b776cb778abe9615b99dca7a737b59ce40213
JoseBambora <zebragapt@gmail.com>
2022-12-11 11:48:13 +0000

    Minor Cahange

--- Etapa3/codigo/src/DAOCONFIG/DAOconfig.java
+++ Etapa3/codigo/src/DAOCONFIG/DAOconfig.java
@@ -1,3 +1,5 @@
+package DAOCONFIG;
+
 public class DAOconfig {
     static final String USERNAME = "";                       // Actualizar
     static final String PASSWORD = "";                       // Actualizar
@@ -5,6 +7,4 @@ public class DAOconfig {
     //private static final String DRIVER = "jdbc:mariadb";        // Usar para MariaDB
     private static final String DRIVER = "jdbc:mysql";        // Usar para MySQL
     static final String URL = DRIVER+"://localhost:3306/"+DATABASE+"?allowPublicKeyRetrieval=true&useSSL=false";
-
-
 }
--- Etapa3/codigo/src/PackageCarro/CarroDAO.java
+++ Etapa3/codigo/src/PackageCarro/CarroDAO.java
@@ -1,10 +1,59 @@
 package PackageCarro;
 
+import DAOCONFIG.DAOconfig;
+import java.sql.*;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
+
 public class CarroDAO implements Map<String,Carro> {
+	private CarroDAO()
+	{
+		try (Connection conn = DriverManager.getConnection(DAOconfig.URL, DAOconfig.USERNAME, DAOconfig.PASSWORD);
+			 Statement stm = conn.createStatement()) {
+			String sql = "CREATE TABLE IF NOT EXISTS salas (" +
+					"Num varchar(10) NOT NULL PRIMARY KEY," +
+					"Edificio varchar(45) DEFAULT NULL," +
+					"Capacidade int(4) DEFAULT 0)";
+			stm.executeUpdate(sql);
+			sql = "CREATE TABLE IF NOT EXISTS turmas (" +
+					"Id varchar(10) NOT NULL PRIMARY KEY," +
+					"Sala varchar(10) DEFAULT NULL," +
+					"foreign key(Sala) references salas(Num))";
+			stm.executeUpdate(sql);
+			sql = "CREATE TABLE IF NOT EXISTS alunos (" +
+					"Num varchar(10) NOT NULL PRIMARY KEY," +
+					"Nome varchar(45) DEFAULT NULL," +
+					"Email varchar(45) DEFAULT NULL," +
+					"Turma varchar(10), foreign key(Turma) references turmas(Id))";
+			stm.executeUpdate(sql);
+		} catch (SQLException e) {
+			// Erro a criar tabela...
+			e.printStackTrace();
+			throw new NullPointerException(e.getMessage());
+		}
+	}
+	/*
+	"CREATE SCHEMA IF NOT EXISTS `simuladorDSS` ;
+	USE `simuladorDSS` ;
+
+	CREATE TABLE IF NOT EXISTS `simuladorDSS`.`ModoMotor` (
+			`id` INT NOT NULL,
+	PRIMARY KEY (`id`))
+	ENGINE = InnoDB;
+
+	CREATE TABLE IF NOT EXISTS `simuladorDSS`.`Motor` (
+			`id` INT NOT NULL,
+			`potencia` INT NOT NULL,
+			`capacidadeCombustivel` INT NOT NULL,
+			`modo` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `modo_idx` (`modo` ASC) VISIBLE,
+	CONSTRAINT `modo`
+	FOREIGN KEY (`modo`)
+	REFERENCES `simuladorDSS`.`ModoMotor` (`id`)"
+	 */
 	@Override
 	public int size() {
 		return 0;
