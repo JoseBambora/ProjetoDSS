19c93666d071166903e8acfddd17ec74cfdeb7ab
Inês Castro <73346645+graphics-ins@users.noreply.github.com>
2022-12-24 02:07:30 +0000

    Ver melhor

--- Etapa3/codigo/src/PackageCircuito/Caracteristica.java
+++ Etapa3/codigo/src/PackageCircuito/Caracteristica.java
@@ -3,8 +3,23 @@ package PackageCircuito;
 public class Caracteristica implements ICaracteristica {
 	private int _gdu;
 	public Circuito _unnamed_Circuito_;
+	
+	// o circuito tem 10 rectas e apresenta a lista de curvas e rectas de modo a que 
+	// o José indique o grau de dificuldade de ultrapassagem (GDU) em cada uma 
+	// (as chicanes têm sempre um GDU de difícil). Para as rectas 1 e 6, e curvas 2 e 3,
+	// indica um GDU de possível. Para as rectas 4, 5, 7 e 8, e curvas 4, 5, 7 e 8,
+	// indica um GDU de impossível. Para os restantes, indica um GDU de difícil.
+	
+	// 1 - possivel
+	// 2 - dificil
+	// 3 - impossivel
 
 	public Boolean validaGDU(Integer aGdu) {
-		throw new UnsupportedOperationException();
+		if{aGdu != 1 || aGdu != 2 || aGdu != 3}
+			return False;
+		else
+		{
+			return True;
+		}
 	}
 }
